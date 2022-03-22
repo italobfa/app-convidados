a3ca927b8de35a666935008b3713cb31afe143a1
italobfa <italobfa@hotmail.com>
2022-03-21 22:38:24 -0300

    cria banco de dados

---
+++ app/src/main/java/com/example/convidados/service/repository/GuestDataBaseHelper.kt
@@ -0,0 +1,29 @@
+package com.example.convidados.service.repository
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.example.convidados.service.constants.DataBaseConstants
+
+class GuestDataBaseHelper(context: Context): SQLiteOpenHelper(context, NAME,null, VERSION) {
+    //manipulacao para novos usuarios
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(CREATE_TABLE_GUEST)
+    }
+    //manipulacao necessaria para quem ja tem o aplicativo
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+
+    }
+
+    companion object {
+        private const val VERSION = 1
+        private const val NAME = "Convidados.db"
+
+        private const val CREATE_TABLE_GUEST = (
+                "create table" + DataBaseConstants.GUEST.TABLE_NAME +"("
+                + DataBaseConstants.GUEST.COLUMNS.ID + "integer primary key autoincrement, "
+                + DataBaseConstants.GUEST.COLUMNS.NAME + "text, "
+                + DataBaseConstants.GUEST.COLUMNS.PRESENCE + "integer);"
+                )
+    }
+}
