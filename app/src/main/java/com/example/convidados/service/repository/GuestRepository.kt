1dbfa9f02431e235864fa8ea337d8e63b2c72f6c
italobfa <italobfa@hotmail.com>
2022-03-22 16:27:47 -0300

    adiciona singleton

--- app/src/main/java/com/example/convidados/service/repository/GuestRepository.kt
+++ app/src/main/java/com/example/convidados/service/repository/GuestRepository.kt
@@ -1,8 +1,25 @@
 package com.example.convidados.service.repository
 
+import android.content.Context
 import com.example.convidados.service.model.GuestModel
 
-class GuestRepository {
+class GuestRepository private constructor(context: Context){
+
+    private var mGuestDataBaseHelper: GuestDataBaseHelper = GuestDataBaseHelper(context)
+    //Singleton
+    companion object{
+        private lateinit var repository: GuestRepository
+
+        fun getInstance(context: Context): GuestRepository{
+            if(!::repository.isInitialized){
+                repository = GuestRepository(context)
+            }
+            return repository
+        }
+
+
+    }
+
     fun getAll(): List<GuestModel>{
         val list: MutableList<GuestModel> = ArrayList()
         return list
