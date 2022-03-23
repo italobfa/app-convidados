79929e254ca06059b99a208888929a47ca92c00c
italobfa <italobfa@hotmail.com>
2022-03-22 22:30:00 -0300

    implementa funcoes do repository

--- app/src/main/java/com/example/convidados/service/model/GuestModel.kt
+++ app/src/main/java/com/example/convidados/service/model/GuestModel.kt
@@ -2,6 +2,6 @@ package com.example.convidados.service.model
 
 data class GuestModel(
     val id: Int = 0,
-    val name: String,
+    val name: String?,
     var presence: Boolean
 )
--- app/src/main/java/com/example/convidados/service/repository/GuestRepository.kt
+++ app/src/main/java/com/example/convidados/service/repository/GuestRepository.kt
@@ -2,6 +2,9 @@ package com.example.convidados.service.repository
 
 import android.content.ContentValues
 import android.content.Context
+import androidx.core.database.getIntOrNull
+import androidx.core.database.getShortOrNull
+import androidx.core.database.getStringOrNull
 import com.example.convidados.service.constants.DataBaseConstants
 import com.example.convidados.service.model.GuestModel
 import java.lang.Exception
@@ -22,19 +25,152 @@ class GuestRepository private constructor(context: Context) {
         }
     }
 
+    fun get(id: Int): GuestModel? {
+
+        var guest: GuestModel? = null
+
+        return try {
+            val db = mGuestDataBaseHelper.readableDatabase
+
+            val projection = arrayOf(
+                DataBaseConstants.GUEST.COLUMNS.NAME,
+                DataBaseConstants.GUEST.COLUMNS.PRESENCE
+            )
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(id.toString())
+
+            val cursor = db.query(
+                DataBaseConstants.GUEST.TABLE_NAME,
+                projection,
+                selection,
+                args,
+                null,
+                null,
+                null
+            )
+
+            if (cursor != null && cursor.count > 0) {
+                cursor.moveToFirst()
+
+                val name =
+                    cursor.getStringOrNull(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.NAME))
+                //Tratando o valor para retornar um boolean
+                val presence =
+                    (cursor.getIntOrNull(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.PRESENCE)) == 1)
+
+                guest = GuestModel(id, name, presence)
+            }
+            cursor?.close()
+            guest
+        } catch (e: Exception) {
+            guest
+        }
+    }
+
     fun getAll(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
-        return list
+
+        return try {
+            val db = mGuestDataBaseHelper.readableDatabase
+
+            val projection = arrayOf(
+                DataBaseConstants.GUEST.COLUMNS.ID,
+                DataBaseConstants.GUEST.COLUMNS.NAME,
+                DataBaseConstants.GUEST.COLUMNS.PRESENCE
+            )
+
+            val cursor = db.query(
+                DataBaseConstants.GUEST.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null
+            )
+
+            if (cursor != null && cursor.count > 0) {
+                //enquanto tiver um proximo valor no cursor, ele vai retorna verdadeiro e continuar ata retornar falso
+                while (cursor.moveToNext()) {
+
+                    val id =
+                        cursor.getIntOrNull(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.ID))
+                    val name =
+                        cursor.getStringOrNull(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.NAME))
+                    val presence =
+                        (cursor.getIntOrNull(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.PRESENCE)) == 1)
+                    //tratei de forma diferente o ID, pois ele nunca vai ser nulo por causa do autoincrement do banco de dados
+                    val guest = GuestModel(id!!, name, presence)
+                    list.add(guest)
+                }
+                cursor.moveToFirst()
+            }
+            cursor?.close()
+            list
+        } catch (e: Exception) {
+            list
+        }
     }
 
     fun getPresent(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
-        return list
+        return try {
+            val db = mGuestDataBaseHelper.readableDatabase
+            //Atencao ao usar o rawQuery e escrever o SQL direto
+            val cursor = db.rawQuery("SELECT id, name, presence FROM Guest WHERE presence = 1", null)
+
+            if (cursor != null && cursor.count > 0) {
+                //enquanto tiver um proximo valor no cursor, ele vai retorna verdadeiro e continuar ata retornar falso
+                while (cursor.moveToNext()) {
+
+                    val id =
+                        cursor.getIntOrNull(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.ID))
+                    val name =
+                        cursor.getStringOrNull(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.NAME))
+                    val presence =
+                        (cursor.getIntOrNull(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.PRESENCE)) == 1)
+                    //tratei de forma diferente o ID, pois ele nunca vai ser nulo por causa do autoincrement do banco de dados
+                    val guest = GuestModel(id!!, name, presence)
+                    list.add(guest)
+                }
+                cursor.moveToFirst()
+            }
+            cursor?.close()
+            list
+        } catch (e: Exception) {
+            list
+        }
     }
 
     fun getAbsent(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
-        return list
+        return try {
+            val db = mGuestDataBaseHelper.readableDatabase
+            //Atencao ao usar o rawQuery e escrever o SQL direto
+            val cursor = db.rawQuery("SELECT id, name, presence FROM Guest WHERE presence = 0", null)
+
+            if (cursor != null && cursor.count > 0) {
+                //enquanto tiver um proximo valor no cursor, ele vai retorna verdadeiro e continuar ata retornar falso
+                while (cursor.moveToNext()) {
+
+                    val id =
+                        cursor.getIntOrNull(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.ID))
+                    val name =
+                        cursor.getStringOrNull(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.NAME))
+                    val presence =
+                        (cursor.getIntOrNull(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.PRESENCE)) == 1)
+                    //tratei de forma diferente o ID, pois ele nunca vai ser nulo por causa do autoincrement do banco de dados
+                    val guest = GuestModel(id!!, name, presence)
+                    list.add(guest)
+                }
+                cursor.moveToFirst()
+            }
+            cursor?.close()
+            list
+        } catch (e: Exception) {
+            list
+        }
     }
 
     fun save(guest: GuestModel): Boolean {
@@ -83,5 +219,4 @@ class GuestRepository private constructor(context: Context) {
             false
         }
     }
-}
 }
