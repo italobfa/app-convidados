5247b29be15b889eab1b799c30b47f53811dc213
italobfa <italobfa@hotmail.com>
2022-03-24 10:04:28 -0300

    implementa o build do banco

---
+++ app/src/main/java/com/example/convidados/service/repository/GuestDataBase.kt
@@ -0,0 +1,21 @@
+package com.example.convidados.service.repository
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+abstract class GuestDataBase : RoomDatabase() {
+    //singleton
+    companion object {
+
+        private lateinit var INSTANCE: GuestDataBase
+
+        fun getDatabase(context: Context): GuestDataBase {
+            if (!::INSTANCE.isInitialized) {
+                INSTANCE = Room.databaseBuilder(context, GuestDataBase::class.java, "guestDB")
+                    .allowMainThreadQueries().build()
+            }
+            return INSTANCE
+        }
+    }
+}
