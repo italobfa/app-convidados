416184190ebb63a501603a30e368c4c6404ba7f5
italobfa <italobfa@hotmail.com>
2022-03-24 09:56:29 -0300

    inicia implementacao do room no app

--- app/src/main/java/com/example/convidados/service/model/GuestModel.kt
+++ app/src/main/java/com/example/convidados/service/model/GuestModel.kt
@@ -1,7 +1,21 @@
 package com.example.convidados.service.model
 
-data class GuestModel(
-    val id: Int = 0,
-    val name: String = "",
-    var presence: Boolean
-)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//o room vai usar os atributos das entidades, para gerar a tabela.
+
+@Entity(tableName = "Guest")
+class GuestModel {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Int = 0
+
+    @ColumnInfo(name = "name")
+    var name: String = ""
+
+    @ColumnInfo(name = "presence")
+    var presence: Boolean = true
+}
